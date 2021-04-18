1130778f247e274f314ed69a84368f973eab77a2
pedrofrohmut <pedrofrohmut@gmail.com>
2021-04-18 13:01:49 -0300

    Hello Heroku

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
-const express = require("express")
+import express from "express"
 
 const App = express()
 
 const PORT = process.env.PORT || 5000
 
 App.get("/", (_, res) => {
-  res.send("Hello World")
+  res.send("Hello Heroku with Typescript")
 })
 
 App.listen(PORT, () => console.log(`Corte em Casa API started on Port: ${PORT}`))

4d54c325c68407c4765d4347c2918c396abb7696
pedrofrohmut <pedrofrohmut@gmail.com>
2021-04-21 15:27:01 -0300

    tsc output dir rename

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: "@react-native-community",
+  rules: {
+    "prettier/prettier": "off",
+    semi: ["warn", "never"],
+    quotes: ["warn", "double"],
+    "comma-dangle": ["error", "never"],
+    curly: "off",
+    "react-native/no-inline-styles": "off"
+  }
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,17 @@
 import express from "express"
+import dotenv from "dotenv"
+import path from "path"
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env")
+})
 
 const App = express()
 
 const PORT = process.env.PORT || 5000
 
 App.get("/", (_, res) => {
-  res.send("Hello Heroku with Typescript")
+  res.send("Welcome to Corte em Casa API")
 })
 
 App.listen(PORT, () => console.log(`Corte em Casa API started on Port: ${PORT}`))

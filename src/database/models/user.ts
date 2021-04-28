f5492c09064ec9ffab0987b95ef77be368641e18
pedrofrohmut <pedrofrohmut@gmail.com>
2021-04-28 13:59:56 -0300

    Model with typescript class

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,26 +1,41 @@
-import { DataTypes } from "sequelize"
+import { DataTypes, Model, Optional } from "sequelize"
 
 import sequelize from "../sequelize-instance"
 
-const UserModel = sequelize.define(
-  "user",
-  {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.VIRTUAL,
-    password_hash: DataTypes.STRING
-  },
-  {
-    // hooks: {
-    //   beforeValidate: async (user, options) => {
-    //     if (user.password) {
-    //       user.password_hash = await bcrypt.hash(user.password, 8)
-    //     }
-    //   }
-    // },
-    underscored: true,
-    timestamps: true
-  }
-)
+interface UserAttributes {
+  id?: string
+  name: string
+  email: string
+  password?: string
+  password_hash: string
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
+
+class UserModel
+    extends Model<UserAttributes, UserCreationAttributes>
+    implements UserAttributes {
+  id?: string
+  name!: string
+  email!: string
+  password?: string
+  password_hash!: string
+}
+
+const attributes = {
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.VIRTUAL,
+  password_hash: DataTypes.STRING
+}
+
+const options = {
+  underscored: true,
+  timestamps: true,
+  sequelize,
+  tableName: "users"
+}
+
+UserModel.init(attributes, options)
 
 export default UserModel
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,35 @@
 import App from "../app"
 import { testConnection } from "./utils"
 
+import UserModel from "../database/models/user"
+
 ;(async () => {
   await testConnection()
+
+  try {
+    await UserModel.create({
+      name: "Jane Doe",
+      email: "jane@doe.com",
+      password_hash: "123456"
+    })
+    console.log("!!! USER CREATED !!!")
+
+    await UserModel.create({
+      name: "John Doe",
+      email: "john@doe.com",
+      password_hash: "123456"
+    })
+    console.log("!!! USER CREATED !!!")
+  } catch (err) {
+    console.error("Error to create a user", err)
+  }
+
+  try {
+    await UserModel.destroy({ truncate: true, force: true, cascade: true })
+    console.log("!!! USERS TABLE TRUNCATED !!!")
+  } catch (err) {
+    console.error("Error to truncate users table", err)
+  }
 })()
 
 if (!process.env.PORT) {
